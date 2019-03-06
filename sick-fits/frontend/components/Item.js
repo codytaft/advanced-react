40f9cfdaab80168baaaf47c91425efdae9b428a5
Cody Taft <cody.taft@gmail.com>
2019-03-05 18:27:52 -0700

    Change button to DeleteItem component in item.js

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -5,6 +5,7 @@ import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
+import DeleteItem from './DeleteItem';
 
 class Item extends Component {
   render() {
@@ -27,7 +28,7 @@ class Item extends Component {
 
         <p>{item.description}</p>
 
-        <div className="buttonList">
+        <div className='buttonList'>
           <Link
             href={{
               pathname: 'update',
@@ -37,7 +38,7 @@ class Item extends Component {
             <a>Edit</a>
           </Link>
           <button>Add to Cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete this item</DeleteItem>
         </div>
       </ItemStyles>
     );
